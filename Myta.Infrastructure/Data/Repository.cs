1e832bd8e0160128cb960f2315b2c170c2ad76df
Nguyen Anh Do <nguyenanhdo200213@gmail.com>
2023-12-07 20:28:46 +0700

    Dựng cấu trúc solution

---
+++ Myta.Infrastructure/Data/Repository.cs
@@ -0,0 +1,13 @@
+﻿using Myta.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myta.Infrastructure.Data
+{
+    public class Repository : IRepository
+    {
+    }
+}
